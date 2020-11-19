7778ee02bc6eccd145099e5ce004cfc1c3335da7
Saurabh <saurabh.nit20@gmail.com>
2020-11-20 00:42:11 +0530

    [Saurabh]. Regex Check

---
+++ RegexValidation.js
@@ -0,0 +1,27 @@
+﻿//RegexJS Demo
+
+function CheckNameUsingRegex(name) {
+    let nameRegex = new RegExp(/^[A-Z][a-z]{2,}$/);
+
+    if (nameRegex.test(name)) {
+        console.log("Valid Name");
+    }
+    else {
+        console.log("Invalid Name");
+    }
+}
+
+function CheckIDUsingRegex(id) {
+    let idRegex = new RegExp(/^[1-9][0-9]*$/);
+
+    if (idRegex.test(id)) {
+        console.log("Valid ID");
+    }
+    else {
+        console.log("Invalid ID");
+    }
+}
+
+
+CheckNameUsingRegex(100);
+CheckNameUsingRegex(0);
